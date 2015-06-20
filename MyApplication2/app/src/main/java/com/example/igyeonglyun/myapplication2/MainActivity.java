8634bcd6b905e89a52ba7848fbacfa26becf27e1
dragonist <erin314@naver.com>
2015-06-20 19:30:06 +0900

    [DAY1] widget & layout

---
+++ MyApplication2/app/src/main/java/com/example/igyeonglyun/myapplication2/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.igyeonglyun.myapplication2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+//            case R.id.button:
+//                Toast.makeText(getApplicationContext(), "버튼을 터치하였습니다.", Toast.LENGTH_SHORT).show();
+//                break;
+        }
+    }
+}
